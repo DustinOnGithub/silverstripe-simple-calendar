ca93019de9a9bc3e68375a7bc7cc7ba0e65b5a3e
Benedikt Hofstätter <hofstaetter.benni@gmail.com>
2017-09-11 14:26:05 +0200

    changed resp. options

--- js/simple-calendar.js
+++ js/simple-calendar.js
@@ -28,26 +28,13 @@ jQuery('document').ready(function() {
     windowResize: function(view) {
       if ($(window).width() < window.simplecalendarBreakpoint){
         $('.calendar.calendar--calendar-view').fullCalendar('changeView', 'listMonth');
-        $('.fc-today-button').hide();
-        $('.fc-right').not('.fc-center').hide();
-        $('.fc-center').addClass('fc-right');
       } else {
-        $('.calendar.calendar--calendar-view').fullCalendar('changeView', 'month');
-        $('.fc-today-button').show();
-        $('.fc-right').show();
-        $('.fc-center').removeClass('fc-right');
+	      $('.calendar.calendar--calendar-view').fullCalendar('changeView', 'month');
       }
     }
   };
 
-  window.simplecalendarOptions.defaultView = $('.calendar.calendar--calendar-view').data('default-view');
-
-  if($(window).width() < window.simplecalendarBreakpoint) {
-    window.simplecalendarOptions.defaultView = 'listMonth';
-    window.simplecalendarOptions.header.left = 'prev';
-    window.simplecalendarOptions.header.right = 'next';
-  }
-
+	window.simplecalendarOptions.defaultView = $('.calendar.calendar--calendar-view').data('default-view');
   $('.calendar.calendar--calendar-view').fullCalendar(window.simplecalendarOptions);
 });
 
